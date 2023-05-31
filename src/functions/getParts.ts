12111a5db4c22f4bd0dd4b88ef431deb79907fc4
Fynn Haupt <fynn.haupt@fynnhaupt.de>
2023-05-31 23:40:51 +0200

    fixed but in getParts.ts

--- src/functions/getParts.ts
+++ src/functions/getParts.ts
@@ -1,12 +1,35 @@
+const partsVariants = [
+  'EVENT',
+  'ENTRY',
+  'ENTRYREMOVE',
+  'SESSION',
+  'SESSIONSTATUS',
+  'WEATHER',
+  'SESSIONENTRY',
+  'DRIVERSTATUS',
+  'BESTLAP',
+  'LASTLAP',
+  'PENALTY',
+  'LAP',
+  'SPLIT',
+  'SPEED',
+  'CLASSIFICATION',
+  'CHALLENGEDATA',
+  'TRACKDATA',
+  'TRACKSEGMENT',
+  'TRACKPOSITION',
+  'CONTACT',
+  'END',
+]
+
 export function getParts(arr: string[]): string[][] {
   const parts: string[][] = [];
 
-  while(arr.length !== 0) {
-    while(arr[0] === '')
-      arr.splice(0, 1);
-
-    const endIndex = arr.findIndex((value, index) => value === '' && arr[index + 1] !== '');
-    parts.push(arr.splice(0, endIndex));
+  while(arr.length > 0) {
+    const nextPartIndex = arr.findIndex((value, index) => partsVariants.indexOf(value) !== -1 && index > 0);
+    const deleteAmount = nextPartIndex === -1 ? arr.length - 1 : nextPartIndex - 1;
+    parts.push(arr.splice(0, deleteAmount));
+    arr.splice(0, 1);
   }
 
   return parts.filter(part => part.length > 0);
